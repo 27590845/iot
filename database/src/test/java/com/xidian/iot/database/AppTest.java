592a29b73e60f6f52dc2d1198c63245eb6de8737
liuyinggang <mrl3141592@163.com>
2020-09-07 08:57:52 +0800

    database模块加入mybatis-spring、spring-jdbc common-mq、common-cache、database提供基础sping配置，可直接"import"至application-context.xml

--- database/src/test/java/com/xidian/iot/database/AppTest.java
+++ database/src/test/java/com/xidian/iot/database/AppTest.java
@@ -2,11 +2,24 @@ package com.xidian.iot.database;
 
 import static org.junit.Assert.assertTrue;
 
+import com.github.pagehelper.PageHelper;
+import com.xidian.iot.database.mapper.SceneMapper;
+import com.xidian.iot.database.pojo.Scene;
+import com.xidian.iot.database.pojo.SceneExample;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+//@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:spring/application-mybatis-def.xml"})
 public class AppTest 
 {
     /**
@@ -16,5 +29,15 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Resource
+    SceneMapper sceneMapper;
+
+    @Test
+    public void getScene(){
+        PageHelper.startPage(0,0);
+        List<Scene> scenes = sceneMapper.selectByExample(new SceneExample());
+        System.out.println(scenes.size());
     }
 }
